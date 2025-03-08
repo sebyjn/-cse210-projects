3fa5fec08ad0eada4ee8e645497c07bd07e814c0
sebyjn <sebyjn4@gmail.com>
2025-03-07 19:57:56 -0500

    Add word

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.Write("What is your favorite IRIRUIJFJFJFJFJFJFJFJFJF  color? ");
     }
 }
