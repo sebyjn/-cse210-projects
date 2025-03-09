d22dee6feb53c4a6912a7b0c1882e78895acfdaa
sebyjn <159225615+sebyjn@users.noreply.github.com>
2025-03-09 00:36:22 -0500

    I finished with the exercie3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
+// in this code we will ask users which will aks for number
 using System;
 
-class Program
+public class HelloWorld
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // We are going to add a random number generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); // Generate a number between 1 and 100
+
+        int guess = -1;
+
+        // Ask the user for a guess until they get it right
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine()); // Read and convert the user input to an integer
+
+            // Provide feedback if the guess is lower or higher than the magic number
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the right number.");
+
+                boolean PlayAgain = false;
+            }
+        }
     }
 }
