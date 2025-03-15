105a8f1dd53a8328c4ec5410bf5da0ad82ca50c2
sebyjn <159225615+sebyjn@users.noreply.github.com>
2025-03-15 01:17:41 -0400

    I finished with journal exercice in week2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,81 @@
+
 using System;
 
-class Program
+public class HelloWorld
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the journal program");
+
+        // Display options to the user
+        Console.WriteLine("Choose one of these options below:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+
+        // Prompt user for their best activity for today
+        Console.WriteLine(" Date: 3/14/2025- prompt: What is your best activity for today?");
+        // Get user input for activity
+        string activityResponse = Console.ReadLine();
+        
+        
+         // Display options to the user
+        Console.WriteLine("Choose one of these options below:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+        
+         // Prompt user for their best activity for today
+        Console.WriteLine("Date: 3/14/2025- prompt: What things I will and not do today?");
+        // Get user input for things
+        string thingsResponse = Console.ReadLine();
+        
+    // Display options to the user
+        Console.WriteLine("Choose one of these options below:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+
+        // Prompt user for modification
+        Console.WriteLine("Date: 3/14/2025- prompt: What modification did I come up with today?");
+        // Get user input for modification
+        string modificationResponse = Console.ReadLine();
+        
+         // Display options to the user
+        Console.WriteLine("Choose one of these options below:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+        
+        
+        
+        // Prompt user for modification
+        Console.WriteLine("Date: 3/14/2025- prompt: what is my first plan for today?");
+        // Get user input for firstplan
+        string firstplanResponse = Console.ReadLine();
+        
+         // Display options to the user
+        Console.WriteLine("Choose one of these options below:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+        
+        
+        // Prompt user for modification
+        Console.WriteLine("Date: 3/14/2025- prompt: which podcast am I going to listen to today?");
+        // Get user input for listen
+        string listenResponse = Console.ReadLine();
+        
+        
     }
 }
